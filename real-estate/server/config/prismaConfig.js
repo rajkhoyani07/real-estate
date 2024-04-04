cb4d94f58043587768902e9c7d1e28310d0b1c48
first project <rajkhoyani152@gmail.com>
2024-04-04 18:28:44 +0530

    real-estate

---
+++ real-estate/server/config/prismaConfig.js
@@ -0,0 +1,9 @@
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
+
+// export {prisma}
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export {prisma}
