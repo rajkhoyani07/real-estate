bd3b6fe1be0bed68bdc085f7a4f8d36c6288a7a2
first project <rajkhoyani152@gmail.com>
2024-04-05 14:02:02 +0530

    api

--- real-estate/client/src/utils/api.js
+++ real-estate/client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://full-stack-real-estate-server-two.vercel.app",
+  baseURL: "https://full-stack-real-estate-rosy.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
